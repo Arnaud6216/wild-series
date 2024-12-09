d0c9eec2290febc42f07f7ebab2ce318bfe96fa9
Arnaud6216 <guevaer8@gmail.com>
2024-12-09 11:24:28 +0100

    adding categories actions and routes

---
+++ server/src/modules/category/categoryActions.ts
@@ -0,0 +1,62 @@
+// Some data to make the trick
+
+
+const categories = [
+
+    {
+  
+      id: 1,
+  
+      name: "Comédie",
+  
+    },
+  
+    {
+  
+      id: 2,
+  
+      name: "Science-Fiction",
+  
+    },
+  
+  ];
+  
+  
+  // Declare the actions
+  
+  
+  import type { RequestHandler } from "express";
+
+  const browse: RequestHandler = (req, res) => {
+      
+     res.json(categories);
+  }
+
+
+
+  const read: RequestHandler = (req, res) => {
+  
+    const parsedId = Number.parseInt(req.params.id);
+
+
+    const categorie = categories.find((c) => c.id === parsedId);
+  
+  
+    if (categorie != null) {
+  
+      res.json(categorie);
+  
+    } else {
+  
+      res.json(categories);
+  
+    }
+  
+  };
+  
+  
+  
+  // Export them to import them somewhere else
+  
+  
+  export default { browse, read };
